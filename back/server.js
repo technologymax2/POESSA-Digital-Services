e0e2568e47fb652b8d246534606f0676c0ad3717
technologymax2 <technologymax2@gmail.com>
2026-06-11 08:56:36 +0300

    kjk

--- back/server.js
+++ back/server.js
@@ -187,7 +187,7 @@ app.use(
   )
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 server.listen(PORT, () => {
   console.log(`Server Running On Port ${PORT}`);
