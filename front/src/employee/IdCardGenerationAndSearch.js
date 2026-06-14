3ea8a07e13f6250db0b76457c603d20d234f9594
technologymax2 <technologymax2@gmail.com>
2026-06-14 21:28:36 +0300

    Update IdCardGenerationAndSearch.js

--- front/src/employee/IdCardGenerationAndSearch.js
+++ front/src/employee/IdCardGenerationAndSearch.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { QRCodeSVG } from 'qrcode.react';
-import './PensionerRegistration.css';
+import './IdCardGenerationAndSearch.css';
 
 function IdCardGenerationAndSearch() {
   const navigate = useNavigate();
