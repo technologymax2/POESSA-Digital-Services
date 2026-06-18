2f403dae92e4fbb148ab0313de932c91e42395bc
technologymax2 <technologymax2@gmail.com>
2026-06-18 09:13:00 +0300

    Update ScanVerify.js

--- front/src/employee/ScanVerify.js
+++ front/src/employee/ScanVerify.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import axios from 'ajax';
+import axios from 'axios';
+
 import './ScanVerify.css';
 
 function ScanVerify() {
