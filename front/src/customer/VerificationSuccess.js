8ac8b17be18bea083cd63ba619d7eabf92f89157
technologymax2 <technologymax2@gmail.com>
2026-06-19 14:01:04 +0300

    Create VerificationSuccess.js

---
+++ front/src/customer/VerificationSuccess.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function VerificationSuccess() {
+
+  return (
+
+    <div style={{ textAlign: "center" }}>
+
+      <h1>
+        Verification Completed Successfully ✅
+      </h1>
+
+      <h3>
+        የሕይወት ማረጋገጫው ተጠናቋል
+      </h3>
+
+    </div>
+
+  );
+
+}
+
+export default VerificationSuccess;
