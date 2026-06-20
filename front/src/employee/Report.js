b9298311e6c2dcedf9fab9fa5d065fe5eeafb046
technologymax2 <technologymax2@gmail.com>
2026-06-20 21:23:40 +0300

    Update Report.js

--- front/src/employee/Report.js
+++ front/src/employee/Report.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../Verification.css"; // የ CSS ስታይሉን እንዲጠቀም
+import "./Report.css"; // የ CSS ስታይሉን እንዲጠቀም
 
 function Report() {
   const [pensioners, setPensioners] = useState([]);
