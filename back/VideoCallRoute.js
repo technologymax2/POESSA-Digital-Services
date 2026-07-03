61c4bf715a10e6b8ef764bd0424698c44f09a495
technologymax2 <technologymax2@gmail.com>
2026-07-03 10:30:50 +0300

    Update VideoCallRoute.js

--- back/VideoCallRoute.js
+++ back/VideoCallRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Pensioner = require("./models/UserPensioner");
+const Pensioner = require("./models/UserPensioner"); // ✅ የስም ካፒታላይዜሽን ተስተካከለ
 
 /* ==================================================
    SEARCH PENSIONER
