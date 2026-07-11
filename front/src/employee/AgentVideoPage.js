6be5ccae203a48944a4afa23518d3ce5e041a51b
technologymax2 <technologymax2@gmail.com>
2026-07-11 13:12:33 +0300

    Update AgentVideoPage.js

--- front/src/employee/AgentVideoPage.js
+++ front/src/employee/AgentVideoPage.js
@@ -128,7 +128,7 @@ const iceConfig = {
       credential: 'ZAggeZx3LEb0xHc4'
     }
   ],
-  iceTransportPolicy: 'relay' // በጣም አስፈላጊው ክፍል ይህ ነው!
+  
 };
 
 // Peer ን በሚያስጀምሩበት ጊዜ ይህንን config ተጠቅመው ያዘምኑት
